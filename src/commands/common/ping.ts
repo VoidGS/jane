190af59d8614d428569d1bdb97a2f0328dd3ec42
VoidGS <voidplay887@gmail.com>
2023-04-06 16:54:37 -0300

    Ajuste no comando

--- src/commands/common/ping.ts
+++ src/commands/common/ping.ts
@@ -1,10 +1,4 @@
-import {
-	ActionRowBuilder,
-	ApplicationCommandType,
-	ButtonBuilder,
-	ButtonStyle,
-	Collection,
-} from 'discord.js'
+import { ApplicationCommandType } from 'discord.js'
 import { Command } from '../../structs/@types/Command'
 
 export default new Command({
@@ -12,24 +6,6 @@ export default new Command({
 	description: 'reply with pong',
 	type: ApplicationCommandType.ChatInput,
 	run({ interaction }) {
-		const row = new ActionRowBuilder<ButtonBuilder>({
-			components: [
-				new ButtonBuilder({
-					customId: 'teste-button',
-					label: 'Clique aqui',
-					style: ButtonStyle.Success,
-				}),
-			],
-		})
-
-		interaction.reply({ ephemeral: true, content: 'pong!', components: [row] })
+		interaction.reply({ ephemeral: true, content: 'pong!' })
 	},
-	buttons: new Collection([
-		[
-			'teste-button',
-			async (interaction) => {
-				interaction.update({ components: [] })
-			},
-		],
-	]),
 })
